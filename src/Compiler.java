9ea79a04156c1eaa52b1710909b767bd42148d2d
iamcon1314 <drakdragon@163.com>
2023-08-27 13:45:10 +0800

    1

---
+++ src/Compiler.java
@@ -0,0 +1,62 @@
+package src;
+
+import src.ASM.backend.InstSelector;
+import src.ASM.backend.RegAllocator;
+import src.Builder.IRBuilder;
+import src.Scope.GlobalScope;
+import src.ast.othernode.ProgramNode;
+import src.grammar.*;
+import src.myerror.MxErrorListener;
+import src.util.*;
+import src.ast.*;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import src.MXIR.*;
+import src.ASM.assembly.*;
+import src.frontend.*;
+//import middleend.*;
+//import src.backend.*;
+
+public class Compiler {
+        public static void main(String[] args) throws Exception {
+                // CharStream input = CharStreams.fromStream(new FileInputStream("input.mx"));
+                // PrintStream irOutput = new PrintStream("output.ll");
+                // PrintStream asmOutput = new PrintStream("output.s");
+                CharStream input = CharStreams.fromStream(System.in);
+                MxLexer lexer = new MxLexer(input);
+                lexer.removeErrorListeners();
+                lexer.addErrorListener(new MxErrorListener());
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                MxParser parser = new MxParser(tokens);
+                parser.removeErrorListeners();
+                parser.addErrorListener(new MxErrorListener());
+                ParseTree tree = parser.program();
+                ASTBuilder astBuilder = new ASTBuilder();
+                ProgramNode ast = (ProgramNode) astBuilder.visit(tree);
+                GlobalScope globalScope = new GlobalScope();
+                new SymbolCollector(globalScope).visit(ast);
+                new SemanticChecker(globalScope).visit(ast);
+                // AST -> LLVM IR
+                // System.setOut(irOutput);
+                IRProgram irProgram = new IRProgram();
+                new IRBuilder(irProgram, globalScope).visit(ast);
+                // System.out.print(irProgram.toString());
+                // LLVM IR -> ASM
+                // System.setOut(asmOutput);
+                ASMModule asmModule = new ASMModule();
+                new InstSelector(asmModule).visit(irProgram);
+                new RegAllocator(asmModule).work();
+
+                new BuiltinAsmPrinter("builtin.s");
+                FileOutputStream out = new FileOutputStream("output.s");
+                out.write(asmModule.toString().getBytes());
+                out.close();
+        }
+}
